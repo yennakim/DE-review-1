e160026bdc1b8193e4983191bdd7cb2ddbaf66a4
Daun Kim <yennakim11@gmail.com>
2025-12-03 20:30:51 -0600

    (3) read an env variable from .env using python-dotenv

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def read_env_variable():
+  NAME = os.getenv('CONTRIBUTOR')
+  print(NAME)
+
 def main():
-  pass
+  read_env_variable()
+  
 
 if __name__ == "__main__":
   main()
